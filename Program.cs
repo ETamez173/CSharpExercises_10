1cdae616a5839b09f04f86beeff6dcb5147e5d09
ETamez173 <56414835+ETamez173@users.noreply.github.com>
2021-03-11 18:17:50 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+
+// Building a Mad Lib | C# | Tutorial 10
+
+namespace CSharpExercises_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // prompt user for input
+
+            string color, pluralNoun, celebrity;
+
+            Console.Write("Enter a color  :");
+            color = Console.ReadLine();
+
+            Console.Write("Enter a plural noun  :");
+            pluralNoun = Console.ReadLine();
+
+            Console.Write("Enter and celebrity  :");
+            celebrity = Console.ReadLine();
+
+            Console.WriteLine("Roses are " + color);
+            Console.WriteLine(pluralNoun + " are blue");
+            Console.WriteLine("I Love "  + celebrity);
+
+            Console.ReadLine();
+
+            int num = 10;
+            num += 100;
+            Console.WriteLine(num);
+
+            int num2 = 10;
+            num2 -= 100;
+            Console.WriteLine(num2);
+
+            int num3 = 10;
+            num3 /= 100;
+            Console.WriteLine(num3);
+
+            int num4 = 10;
+            num4 *= 100;
+            Console.WriteLine(num4);
+
+            num++;
+            Console.WriteLine(num);
+
+            Console.ReadLine();
+
+        }
+    }
+}
